1f4b10a2e414a596bd2530946e34886a1828969a
rur1k <moalmatrafi@gmail.com>
2025-02-18 09:50:09 +0300

    more organized structure

---
+++ src/asm_files/gpio_test.asm
@@ -0,0 +1,14 @@
+nop
+nop
+lui x10, 0x20000
+addi x10, x10, 0x100
+addi x11, x0, 0x10
+sw x11, 4(x10)
+add x0, x0, x0
+nop
+nop
+lui x10, 0x20000
+addi x10, x10, 0x100
+addi x11, x0, 0x1
+sw x11, 4(x10)
+add x0, x0, x0    
