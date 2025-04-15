673cadabdce54b37cf0e2919f000ca3f37ab9a34
Rur1k <moalmatrafi90@gmail.com>
2025-04-15 10:05:09 +0300

    done with the stall and type

--- src/soc/core/datapath/compressed/stall_compressed.sv
+++ src/soc/core/datapath/compressed/stall_compressed.sv
@@ -1,7 +1,10 @@
 module stall_compressed (
-    input  logic clk,
-    input  logic reset_n,
-    input  logic f1f1,
+    input logic clk,
+    input logic reset_n,
+    input logic clear,
+    input logic en,
+    input logic [31:0] pc,
+    input logic f1f1,
     f1f2,
     fh,
     hf,
@@ -10,11 +13,11 @@ module stall_compressed (
     stall_compressed_ff
 );
 
+  assign stall_compressed = (fh | hh) & (~stall_compressed_ff) & (~pc[1]);
 
   always_ff @(posedge clk or negedge reset_n) begin
-    if (!reset_n) stall_compressed_ff <= 1'b0;
-    else stall_compressed_ff <= stall_compressed;
+    if (!reset_n | clear) stall_compressed_ff <= 1'b0;
+    else if (en) stall_compressed_ff <= stall_compressed;
   end
 
-  assign stall_compressed = (fh | hh) & ~(stall_compressed_ff);
 endmodule
