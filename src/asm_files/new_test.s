9f22422204bd065a0882346cf031cade56aabe8f
Rur1k <moalmatrafi90@gmail.com>
2025-04-17 14:54:48 +0300

    c.jal and jal tested

--- src/asm_files/new_test.s
+++ src/asm_files/new_test.s
@@ -14,25 +14,73 @@ sw x0, 4(x21) # load zero to mtimecmp_upper
 
 li x10, 0
 li x11, 0
+li x21, 0
+li x20, 0
+li x25, 0
 
 
 loop:
-c.addi x1, 1
+c.jal adr1 #first half case
+#jump to below
+
+adr1: add x15, x1, x0
+
+c.jal adr2 #second half case
+#jump to below
+adr2: add x16, x1, x0
+
+c.nop
+jal x17, adr3 #second half case for full inst
+c.nop
+
+#-------------------------------------------------
+#jump to below (here multiple of 4)
+adr3: c.nop
+c.nop
+
+c.nop
+c.nop
+
+c.jal adr4 #first half case
+#jump to below
+adr4: add x18, x1, x0
+c.nop
+
+c.nop
+c.nop
+
+c.nop
+c.jal adr5#second half case
+#jump (here multiple of 4)
+adr5: add x19, x1, x0
+c.nop
+c.nop
+c.nop
+c.nop
+c.nop
+jal x20, loop #second half case for full inst
+
+
+
+
+
+
+
 c.addi x2, 1
 c.addi x3, 1
 c.addi x4, 1
 
-c.addi x5, 1
-addi x6, x6, 0x100
-c.addi x7, 1
-
+c.addi x6, 1
+addi x7, x7, 0x100
 c.addi x8, 1
-addi x9, x9, 0x100
-addi x10, x10, 0x100
-c.addi x11, 1
 
-addi x12, x12, 0x100
+c.addi x9, 1
+addi x10, x10, 0x100
+addi x11, x11, 0x100
+c.addi x12, 1
+
 addi x13, x13, 0x100
+addi x14, x14, 0x100
 j loop # executed
 
 
@@ -47,5 +95,3 @@ nop
 nop
 nop
 mret
-
-
