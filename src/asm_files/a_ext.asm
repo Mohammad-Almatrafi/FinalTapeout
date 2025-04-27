777c8102d85c0fa102e16ea2dffa3ac8c45190c4
BinAhmed-Elead <abdullahelead@gmail.com>
2025-04-27 13:34:12 +0300

    working lr sc in simulation

--- src/asm_files/a_ext.asm
+++ src/asm_files/a_ext.asm
@@ -1,20 +1,24 @@
-start:
-        # a0 holds address of memory location
-        # a1 holds expected value
-        # a2 holds desired value
-        # a0 holds return value, 0 if successful, !0 otherwise
-        li a0,0x10000000
-        li a1, 1
-        li a2, 1
-        li a0, 0
-   
-    cas:
-        lr.w t0, 0(a0)        # Load original value.
-        bne t0, a1, fail     # Doesn't match, so fail.
-        sc.w t0, a2, 0(a0)    # Try to update.
-        bnez t0, cas         # Retry if store-conditional failed.
-        li a0, 0             # Set return to success.
-        j start                # Return.
-    fail:
-        li a0, 1             # Set return to failure.
-        j start               # Return.
+.section .init
+main:
+
+    li x5, 0x969600
+    li x6, 0x12
+    sw x6, (x5)
+    li x6, 0xbb
+    nop
+    nop
+    nop
+    nop
+    nop
+    
+    lr.w x7, (x5)      # Load-reserved from data-mem into x5
+    sc.w x9, x6, (x5)
+    nop
+    sc.w x9, x6, (x5)  # Attempt to store x2 into data-mem
+
+    # -------------------------
+    # Done
+    loop:
+    j loop
+    nop
+
